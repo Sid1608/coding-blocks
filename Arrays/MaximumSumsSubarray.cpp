655b2aa8120b5f0dd0b6cf3ec7066b7a3416b94c
Sid1608 <akarsiddharth@gmail.com>
2022-03-13 15:56:48 +0530

    printing maximum sum subarray

--- Arrays/MaximumSumsSubarray.cpp
+++ Arrays/MaximumSumsSubarray.cpp
@@ -12,16 +12,25 @@ void GenerateSubarrays(int a[],int n){
 }
 void MaximumSumSubarray(int a[],int n){
     int max_sum=INT_MIN;
+    int wi,wj;
     for(int i=0;i<n;i++){
         for(int j=i;j<n;j++){
             int sum=0;
             for(int k=i;k<=j;k++){
                 sum+=a[k];
             }
-            max_sum=max(max_sum,sum);
+            if(max_sum<sum){
+                max_sum=sum;
+                wi=i;wj=j;
+            }
+            // max_sum=max(max_sum,sum);
         }
     }
     cout<<max_sum<<endl;
+    cout<<"Maximum Subarray: "<<endl;
+    for(int i=wi;i<=wj+1;i++){
+        cout<<a[i]<<" ";
+    }
 }
 int main() {
 	int a[]={5,3,1,2,4};
