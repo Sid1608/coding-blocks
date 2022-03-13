482ddadb103b8dc56324eb614fe40204c2551ae8
Sid1608 <akarsiddharth@gmail.com>
2022-03-13 16:34:17 +0530

    Character arrays

---
+++ CharacterArrays/Character-Arrays.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main() {
+	char a[7]={'S','I','D','D','H','A','\0'};
+    cout<<a<<endl;
+    char b[10];
+    b[0]='a';
+    b[1]='b';
+    b[2]='\0';
+    cout<<b<<endl;
+    char c[]="welcome to coding blocks";
+    char ch[100]="welcome to coding blocks";
+    cout<<c<<endl<<ch<<endl;
+	return 0;
+}
