bf3f4b5e9c7e1c2cab7df8bae36dec329964c0dc
sid1608 <akarsiddharth@gmail.com>
2022-03-26 11:10:56 +0530

    template

--- template.cpp
+++ template.cpp
@@ -2,13 +2,21 @@
 
 #include <iostream>
 #include<cstring>
+#include<cmath>
 #include<string>
+#include<vector>
+#include<queue>
 #include<algorithm>
 using namespace std;
+#define  ull unsigned long long int
+#define  ll long long int
 
 int main()
 { 
-	int arr[]={5,4,7,8,10};
-    int n=sizeof(arr)/sizeof(int);
+	int t=1;
+	cin>>t;
+	while(t--){
+
+	}
 	return 0;
 }
